ddc7cc2e4d34339e0892f8db260a2676b55db751
garvitarora2403 <garvit.arora@innovasolutions.com>
2025-01-23 11:45:37 +0530

    fourth

--- connect.js
+++ connect.js
@@ -6,19 +6,16 @@ async function connectToMongodb(){
     const MONGO_URI = process.env.MONGO_URI;
 
     const options = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         serverSelectionTimeoutMS: 30000,  // Wait for up to 30 seconds to establish the connection
         socketTimeoutMS: 45000,           // Set socket timeout to 45 seconds for queries
       };
-    
-
+      
       mongoose.connect(MONGO_URI, options)
-  .then(() => console.log("MongoDB connected successfully"))
-  .catch(err => {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1);  // Exit the process if MongoDB connection fails
-  });
+        .then(() => console.log("MongoDB connected successfully"))
+        .catch(err => {
+          console.error("Error connecting to MongoDB:", err);
+          process.exit(1);  // Exit the process if MongoDB connection fails
+        });
 }
 
 
